193218697fe6b12c49e210f6f7b9c380523d9232
Akira Sasaki <a_sasaki@r.recruit.co.jp>
2020-12-02 20:30:28 +0900

    できた！

--- users/views.py
+++ users/views.py
@@ -4,13 +4,19 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
+socket_list = {}
+
 class IndexView(TemplateView):
     template_name = "users/index.html"
 
 async def websocket_view(socket):
     await socket.accept()
-    sum = 0
+    print(type(socket.headers))
+    key = socket.headers['sec-websocket-key']
+    socket.scope["room"] = "test"
+    socket_list[key] = socket
     await socket.send_text("socket start")
+    await socket.send_text(str(socket.scope))
     # while sum < 20:
     #     await socket.receive_text()
     #     sum += random.randint(0,10)
@@ -21,8 +27,10 @@ async def websocket_view(socket):
 
     while True:
         message = await socket.receive_text()
-        message = 'add_' + message
-        await socket.send_text(message)
+        for s in socket_list.values():
+            if s.scope["room"] == "test":
+                await s.send_text(message)
+        # await socket.send_text(message)
     # await socket.send_text(str(socket.headers))
     # await socket.send_text('hello')
     # await socket.close()
